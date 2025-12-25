ef30fa21f6d316613d00e46b061e909b4bb36de8
Googlewarfare <garvinstvillier@gmail.com>
2025-12-25 08:31:04 -0500

    Fix Capacitor appId / config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,24 +1,14 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.83c9e8a99a414ffaabb041590982157d',
-  appName: 'Blossom Dating',
+  appId: 'com.googlewarfare.blossom',
+  appName: 'Blossom',
   webDir: 'dist',
   server: {
     url: 'https://83c9e8a9-9a41-4ffa-abb0-41590982157d.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 2000,
-      launchAutoHide: true,
-      backgroundColor: '#FBD5D5',
-      showSpinner: false,
-      androidScaleType: 'CENTER_CROP',
-      splashFullScreen: true,
-      splashImmersive: true,
-    },
+    cleartext: true,
   },
 };
 
 export default config;
+
